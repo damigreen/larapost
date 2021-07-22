a73ec817339c8dac10b514330d3322fbc99a240c
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-22 22:17:29 +0100

    Registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,37 @@
 
   <div class="flex justify-center">
     <div class="w-8/12 bg-white p-6 rounded-lg">
-      Register
+      <form action="{{ route('register') }}">
+        @csrf
+        <div class="mb-4">
+          <label for="name" class="sr-only">Name</label>
+          <input type="text" name="name" id="name" placeholder="Your name" class='bg-gray-100 border-2 w-full p-4 rounded-lg' value=''>
+        </div>
+
+        <div class="mb-4">
+          <label for="usermane" class="sr-only">Email</label>
+          <input type="text" name="username" id="username" placeholder="Username" class='bg-gray-100 border-2 w-full p-4 rounded-lg' value=''>
+        </div>
+
+        <div class="mb-4">
+          <label for="email" class="sr-only">Username</label>
+          <input type="text" name="email" id="email" placeholder="Your email" class='bg-gray-100 border-2 w-full p-4 rounded-lg' value=''>
+        </div>
+
+        <div class="mb-4">
+          <label for="password" class="sr-only">Password</label>
+          <input type="text" name="password" id="password" placeholder="Password" class='bg-gray-100 border-2 w-full p-4 rounded-lg' value=''>
+        </div>
+
+        <div class="mb-4">
+          <label for="password_confirmation" class="sr-only">Password again</label>
+          <input type="text" name="password_confirmation" id="password_confirmation" placeholder="Repeat your password" class='bg-gray-100 border-2 w-full p-4 rounded-lg' value=''>
+        </div>
+
+        <div>
+          <button type="submit" class="bg-blue-500 text-white px-4 py-3 rounded font-medium w-full">Register</button>
+        </div>
+      </form>
     </div>
   </div>
 
