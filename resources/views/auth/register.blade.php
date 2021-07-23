cebcebcaf058977ebce8e0408478935baec3b291
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-23 21:37:10 +0100

    Modify register page layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,13 +3,18 @@
 @section('content')
 
   <div class="flex justify-center">
-    <div class="w-8/12 bg-white p-6 rounded-lg">
+    <div class="w-4/12 bg-white p-6 rounded-lg">
       <form action="{{ route('register') }}">
         @csrf
         <div class="mb-4">
           <label for="name" class="sr-only">Name</label>
           <input type="text" name="name" id="name" placeholder="Your name" class='bg-gray-100 border-2 w-full p-4 rounded-lg' value=''>
         </div>
+        @error('name')
+          <div class='text-red-500 mt-2 text-sm'>
+            {{ message }}
+          </div>
+        @enderror
 
         <div class="mb-4">
           <label for="usermane" class="sr-only">Email</label>
