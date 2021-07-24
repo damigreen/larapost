eab8e2e45114affe0686b36a3806edc2394c9e93
Damilola Faseun <damilola.faseun@gmail.com>
2021-07-24 19:10:02 +0100

    Create new user in the database

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Auth\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 
 class RegisterController extends Controller 
 {
@@ -17,7 +19,7 @@ class RegisterController extends Controller
         // dd($request);
         // dd($request->get('email'));
         // dd($request->email);
-        
+
         // Validation
         $this->validate($request, [
             // 'name' => ['required', 'max' => 255],
@@ -27,8 +29,12 @@ class RegisterController extends Controller
             'password' => 'required|confirmed',
         ]);
 
-        dd('store');
-
+        User::create([
+            'name' => $request -> name,
+            'email' => $request -> email,
+            'username' => $request -> username,
+            'password' => Hash::make($request -> password),
+        ]);
 
         // store user
         // sign user in
